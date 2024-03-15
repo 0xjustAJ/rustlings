d309ca13962c213be96d95668ab9b55278e132a0
0xjustAJ <justajayi07@gmail.com>
2024-03-15 02:33:56 +0100

    solved excercises on primitive types

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -5,9 +5,9 @@
 // - Functions
 // - If
 //
-// Mary is buying apples. The price of an apple is calculated as follows:
+// damola is buying apples. The price of an apple is calculated as follows:
 // - An apple costs 2 rustbucks.
-// - If Mary buys more than 40 apples, each apple only costs 1 rustbuck!
+// - If damola buys more than 40 apples, each apple only costs 1 rustbuck!
 // Write a function that calculates the price of an order of apples given the
 // quantity bought.
 //

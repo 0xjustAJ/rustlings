65974034720ae5f286a844b494ca6ce85a9dfde7
0xjustAJ <justajayi07@gmail.com>
2024-03-28 23:47:17 +0100

    solved examples on enum

--- exercises/01_variables/variables5.rs
+++ exercises/01_variables/variables5.rs
@@ -7,7 +7,7 @@
 fn main() {
     let mut number: &str = "T-H-R-E-E"; // don't change this line
     println!("Spell a Number : {}", number);
-   let number = 3; // don't rename this variable
+    let number = 3; // don't rename this variable
     println!("Number plus two is : {}", number + 2);
     // Answer:assign the mutability keyword, and add the declaration key "let" to the third line for proper initialization
 }
